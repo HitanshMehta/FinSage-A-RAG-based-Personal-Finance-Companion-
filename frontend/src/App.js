e3c13d90e2a3fd0b74055376464b86e42ee14e66
HitanshMehta <hitanshmehta@gmail.com>
2025-08-20 23:13:53 +0530

    FINSAGE PROJECT

---
+++ frontend/src/App.js
@@ -0,0 +1,55 @@
+
+// import React from "react";
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Chatbot from "./pages/Chatbot";
+// import Dashboard from "./pages/Dashboard";
+// import Navbar from "./components/Navbar";
+// import WhatsHot from "./pages/WhatsHot";
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar /> {/* Always show Navbar since no authentication is needed */}
+//       <div className="container">
+//         <Routes>
+//           <Route path="/" element={<Home />} /> {/* Default to Home page */}
+//           <Route path="/chatbot" element={<Chatbot />} />
+//           <Route path="/dashboard" element={<Dashboard />} />
+//           <Route path="/whats-hot" element={<WhatsHot />} /> {/* New route */}
+//           <Route path="*" element={<Navigate to="/" replace />} /> {/* Redirect invalid routes to Home */}
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Chatbot from "./pages/Chatbot";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+import WhatsHot from "./pages/WhatsHot";
+import StockAnalyzer from "./pages/StockAnalyzer";
+
+function App() {
+  return (
+    <div>
+      <Navbar /> {/* Always show Navbar since no authentication is needed */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Default to Home page */}
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/whats-hot" element={<WhatsHot />} />
+          <Route path="/stock-analyzer" element={<StockAnalyzer />} /> {/* New route */}
+          <Route path="*" element={<Navigate to="/" replace />} /> {/* Redirect invalid routes to Home */}
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
